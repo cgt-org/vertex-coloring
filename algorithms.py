f8ea5a48ab4856bf94f6da3ea7b17885b8f31536
madpeh <48161600+madpeh@users.noreply.github.com>
2021-05-15 17:04:57 +0200

    Reformat

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,30 @@
+from collections import Counter
+import copy
+
+
 def greedy_coloring(vertices, color_limit):
-  if color_limit < 1:
-    print("Error: please input a valid color limit")
-    return
-  for v in vertices:
-    candidate_color = 0
-    v.color_with(candidate_color)
-    while not v.is_properly_colored():
-        candidate_color += 1
+    if color_limit < 1:
+        print("Error: please input a valid color limit")
+        return
+
+    color_counter = Counter()
+
+    for v in vertices:
+        candidate_color = 0
+
+        while color_counter[candidate_color] >= color_limit:
+            candidate_color += 1
+
         v.color_with(candidate_color)
+        while not v.is_properly_colored():
+            candidate_color += 1
+            v.color_with(candidate_color)
+
+        color_counter[candidate_color] += 1
+
+
+def largest_first_coloring(vertices, color_limit):
+    sorted_vertices = sorted(
+        copy.deepcopy(vertices), key=lambda vertex: vertex.get_degree()
+    )
+    greedy_coloring(sorted_vertices, color_limit)
--- test.py
+++ test.py
@@ -1,13 +1,14 @@
-from graph import *
-from algorithms import *
+from graph import Graph, Vertex
+from algorithms import greedy_coloring, largest_first_coloring
+
 
 print("Testing 3-cycle")
 
 test = Graph(3)
 
-test.add_edge(0,1)
-test.add_edge(1,2)
-test.add_edge(2,0)
+test.add_edge(0, 1)
+test.add_edge(1, 2)
+test.add_edge(2, 0)
 
 
 test.color_with(greedy_coloring, 7)
@@ -21,11 +22,11 @@ print("Testing 5-cycle")
 
 five = Graph(5)
 
-five.add_edge(0,1)
-five.add_edge(1,2)
-five.add_edge(2,3)
-five.add_edge(3,4)
-five.add_edge(4,0)
+five.add_edge(0, 1)
+five.add_edge(1, 2)
+five.add_edge(2, 3)
+five.add_edge(3, 4)
+five.add_edge(4, 0)
 
 five.color_with(greedy_coloring, 7)
 print(five.check_proper_coloring(7))
